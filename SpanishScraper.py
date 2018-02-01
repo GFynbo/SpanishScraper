aa7a61f04dac476f347d9e5be3f8437d5f50596d
Gavin Fynbo <gavfynbo@gmail.com>
2018-02-01 11:48:10 -0500

    init repo

---
+++ SpanishScraper.py
@@ -0,0 +1,10 @@
+#
+# Gavin Fynbo
+# © 2018
+# fynbo@bu.edu | gavin@gavinfynbo.com
+#
+
+from bs4 import BeautifulSoup
+
+def main():
+    ''' main function that runs the spanish scraper program '''
